7ae14ca4007bd6fdd5ca242fdadae5fef149e995
Serafina Carlucci <sgc32@duke.edu>
2023-10-01 12:55:25 +0200

    OutOfBounds
    added an empty object with box collider and brief script to reset the scene

--- Assets/Scripts/LevelRestart.cs
+++ Assets/Scripts/LevelRestart.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelRestart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other) // will restart level when collide with box collider
     {
-        
+        //PlatformerController2D controller = other.gameObject.GetComponent<PlatformerController2D>();
+        //if (controller != null)
+ 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
