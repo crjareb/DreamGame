e14d896fcc156607a0b0de1d8d8cd58eb17198b4
Serafina Carlucci <sgc32@duke.edu>
2023-10-04 13:32:51 +0200

    Audio Add Attempt

--- Assets/Scripts/StarCounter.cs
+++ Assets/Scripts/StarCounter.cs
@@ -4,44 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Animations;
 
-public class StarController : MonoBehaviour
+public class StarCounter : MonoBehaviour
 {
     public int counter;
     private Animator animator;
-    //public Sprite[] starSprites;
-    //public Sprite explosion;
-    //public WaitForSeconds switchTime; 
+    public AudioSource myAudioSource;
+     
+
+    private void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Star>() != null)
         {
-            counter++;
+            //counter++;
             animator = other.GetComponent<Animator>();
             animator.SetBool("PlayerCollide", true);
+            myAudioSource.Play();
+
         }
     }
-
-
-    /*public void OnTriggerEnter2D(Collider2D other)
-    {
-        Sprite currSprite = starSprites[counter];
-        if (counter< starSprites.Length)
-        {
-            counter++; 
-        }
-        else
-        {
-            Destroy(transform.gameObject);
-            counter = 0;
-        }
-
-
-        yield return new WaitForSeconds(switchTime);
-        StartCoroutine("SwitchSprite");
-
-        Instantiate(explosion, transform.position, transform.rotation);
-    }*/
 }
         
 
