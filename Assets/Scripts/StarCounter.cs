ce42876b8b292bcc6c542b1197319249efb85b58
Caroline Jareb <cjareb@gmail.com>
2023-10-06 17:13:51 +0200

    Final Star code started

--- Assets/Scripts/StarCounter.cs
+++ Assets/Scripts/StarCounter.cs
@@ -9,14 +9,16 @@ public class StarCounter : MonoBehaviour
     public int counter;
     private Animator animator;
     private AudioSource myAudioSource;
-    //public Sprite[] starSprites;
-    //public Sprite explosion;
-    //public WaitForSeconds switchTime;
+
+    [Header("Level Achievement Settings")]
+    public int numberOfStars;
+    public GameObject finalStar;
 
     private void Start()
     {
         counter = 0;
         myAudioSource = GetComponent<AudioSource>();
+        finalStar.SetActive(false);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -28,6 +30,27 @@ public class StarCounter : MonoBehaviour
             animator = other.GetComponent<Animator>();
             animator.SetBool("PlayerCollide", true);
 
+        }
+
+        if (counter >= numberOfStars)
+        {
+            finalStar.SetActive(true);
+        }
+
+        if (other.GetComponent<FinalStar>() != null)
+        {
+            //animator = other.GetComponent<Animator>();
+            //animator.SetBool("PlayerCollide", true);
+            Invoke("LoadNextScene", 3f);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 }
