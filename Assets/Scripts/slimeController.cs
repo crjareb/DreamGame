07f1de2432bccd54a41d13b4f23b42b4f9f7e918
Serafina Carlucci <sgc32@duke.edu>
2023-10-09 17:13:56 +0100

    L3 completed with sounds

--- Assets/Scripts/slimeController.cs
+++ Assets/Scripts/slimeController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class slimeController : MonoBehaviour
+
+
 {
+    public AudioSource slimeSound;
+
+    private void Start()
+    {
+        slimeSound = GetComponent<AudioSource>();
+    }
     void OnTriggerEnter2D(Collider2D other) // will restart level when collide with box collider
     {
         //PlatformerController2D controller = other.gameObject.GetComponent<PlatformerController2D>();
         //if (controller != null)
-
+        slimeSound.Play(); 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
