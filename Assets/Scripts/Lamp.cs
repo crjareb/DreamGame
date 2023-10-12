779cc091e587c0c045b1c463578602aad93e70c8
Caroline Jareb <cjareb@gmail.com>
2023-10-12 11:05:34 +0200

    FinalScreen updated

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Lamp : MonoBehaviour
 {
 
+    Animator animator;
+    private void Start()
+    {
+        Invoke("LampOn", 1f);
+    }
+
+    public void LampOn()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("LampOn");
+    }
     public void DestroyLamp()
     {
         Destroy(gameObject);
